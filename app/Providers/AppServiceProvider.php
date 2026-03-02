83ac33172b5c25c032099a2ae28e3baab56e5a9c
zu3st-de <simon.langen@gmail.com>
2026-03-02 11:48:32 +0100

    aktueller stand

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('layouts.navigation', function ($view) {
+
+            if (Auth::check()) {
+
+                $activeTournaments = Auth::user()
+                    ->tournaments()
+                    ->where('status', '!=', 'finished')
+                    ->orderBy('created_at')
+                    ->select('id', 'name', 'status')
+                    ->get();
+
+                $view->with('activeTournaments', $activeTournaments);
+            } else {
+                $view->with('activeTournaments', collect());
+            }
+        });
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     <title>{{ config('app.name', 'DartManager') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
